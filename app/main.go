d6c8e412adec6fb608f6912a9eb84d57f4815af9
Luiz Gustavo Bourscheit <luiz.bourscheit@neoway.com.br>
2021-01-28 23:19:48 -0300

    update dockerfiles and readme

--- app/main.go
+++ app/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
-	database "import-file-nwy/db"
+	"os"
 	"time"
+
+	database "github.com/etl-golang/app/db"
 )
 
 const fileName = "base_formatada.txt"
 
 func main() {
+	if _, err := os.Stat(fileName); os.IsNotExist(err) {
+		fmt.Println("ERROR: File not found.")
+		return
+	}
+
 	fmt.Println("Início", time.Now().Format("02/01/2006 15:04:05"))
 
 	database.OpenDB()
